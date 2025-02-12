b4552adc7173e2879428ab7cb71aa9ffd06ffc4a
Valgrif <dgh1288@gmail.com>
2025-02-12 10:28:15 +0000

    Validation Rules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
-
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(
+    //Para validar solicitudes entrantes
+    new ValidationPipe(
+      
+    ) 
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
